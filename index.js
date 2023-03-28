47327d0bdff3c09bb6003aaa2f9c7f3de272def0
Valeri Karpov <val@karpov.io>
2023-03-28 16:53:29 -0400

    initial commit of pwa stuff

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('./public'));
+
+app.listen(process.env.PORT || 3000);
+console.log('Listening on port', process.env.PORT || 3000);
